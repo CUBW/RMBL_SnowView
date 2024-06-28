e2c53f5e429c4a1f1ac1e15e6d8e80365a676027
Andrew Fox <apfox500@gmail.com>
2024-06-28 12:38:02 -0600

    removed ops import

--- DeepLab/Model.py
+++ DeepLab/Model.py
@@ -5,7 +5,7 @@
 # modified code for model to work for our specific use case
 
 import keras
-from keras import layers, models, ops
+from keras import layers, models
 
 def convolution_block(block_input, num_filters=256, kernel_size=3, dilation_rate=1, use_bias=False):
     """
@@ -23,7 +23,7 @@ def convolution_block(block_input, num_filters=256, kernel_size=3, dilation_rate
     """
     x = layers.Conv2D(num_filters, kernel_size=kernel_size, dilation_rate=dilation_rate, padding="same", use_bias=use_bias,kernel_initializer=keras.initializers.HeNormal())(block_input)
     x = layers.BatchNormalization()(x)
-    return ops.nn.relu(x)
+    return layers.Activation('relu')(x)
 
 def DilatedSpatialPyramidPooling(dspp_input):
     """
@@ -38,7 +38,7 @@ def DilatedSpatialPyramidPooling(dspp_input):
     dims = dspp_input.shape
     x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(dspp_input)
     x = convolution_block(x, kernel_size=1, use_bias=True)
-    out_pool = layers.UpSampling2D(size=(dims[-3]//x.shape[1], dims[-2]//x.shape[2]),interpolation="bilnear")(x)
+    out_pool = layers.UpSampling2D(size=(dims[-3]//x.shape[1], dims[-2]//x.shape[2]),interpolation="bilinear")(x)
 
     out_1 = convolution_block(dspp_input, kernel_size=1, dilation_rate=1)
     out_6 = convolution_block(dspp_input, kernel_size=3, dilation_rate=6)
