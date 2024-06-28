fafbbf7efcfcaf0ac1732f9fdb10545432225dae
Andrew Fox <apfox500@gmail.com>
2024-06-28 12:18:50 -0600

    created DeepLab model

---
+++ DeepLab/Model.py
@@ -0,0 +1,55 @@
+# Adapted from: https://github.com/keras-team/keras-io/blob/master/examples/vision/deeplabv3_plus.py
+# Original author: Soumik Rakshit
+# License: Apache License 2.0
+
+# modified code for model to work for our specific use case
+
+import keras
+from keras import layers, models, ops
+
+def convolution_block(block_input, num_filters=256, kernel_size=3, dilation_rate=1, use_bias=False):
+    x = layers.Conv2D(num_filters, kernel_size=kernel_size, dilation_rate=dilation_rate, padding="same", use_bias=use_bias,kernel_initializer=keras.initializers.HeNormal())(block_input)
+    x = layers.BatchNormalization()(x)
+    return ops.nn.relu(x)
+
+def DilatedSpatialPyramidPooling(dspp_input):
+    dims = dspp_input.shape
+    x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(dspp_input)
+    x = convolution_block(x, kernel_size=1, use_bias=True)
+    out_pool = layers.UpSampling2D(size=(dims[-3]//x.shape[1], dims[-2]//x.shape[2]),interpolation="bilnear")(x)
+
+    out_1 = convolution_block(dspp_input, kernel_size=1, dilation_rate=1)
+    out_6 = convolution_block(dspp_input, kernel_size=3, dilation_rate=6)
+    out_12 = convolution_block(dspp_input, kernel_size=3, dilation_rate=12)
+    out_18 = convolution_block(dspp_input, kernel_size=3, dilation_rate=18)
+
+    x = layers.Concatenate(axis=-1)([out_pool, out_1, out_6, out_12, out_18])
+    output = convolution_block(x, kernel_size=1)
+    return output
+
+
+def DeepLabV3Plus(n_classes, img_height, img_width, img_channels):
+    model_input = keras.Input(shape=(img_height, img_width, img_channels))
+    preprocessed = keras.applications.resnet50.preprocess_input(model_input)
+    resnet50 = keras.applications.ResNet50(include_top=False, weights='imagenet', input_tensor=preprocessed)
+    x = resnet50.get_layer("conv4_block6_2_relu").output
+    x = DilatedSpatialPyramidPooling(x)
+
+    input_a = layers.UpSampling2D(size=(img_height // 4 // x.shape[1], img_width // 4 // x.shape[2]), interpolation="bilinear")(x)
+    input_b = resnet50.get_layer("conv2_block3_2_relu").output
+    input_b = convolution_block(input_b, num_filters=48, kernel_size=1)
+
+    x = layers.Concatenate(axis=-1)([input_a, input_b])
+    x = convolution_block(x)
+    x = convolution_block(x)
+    x = layers.UpSampling2D(size=(img_height // x.shape[1], img_width // x.shape[2]), interpolation="bilinear")(x)
+    model_output = layers.Conv2D(n_classes, kernel_size=(1,1), padding="same")(x)
+
+    return keras.Model(inputs=model_input, outputs=model_output)
+
+if __name__ == "__main__":
+    model = DeepLabV3Plus(n_classes=1, img_height=640, img_width=640, img_channels=3)
+    print(f"Input shape: {model.input_shape}")
+    print(f"Output shape: {model.output_shape}")
+    print(f"Trainable params: {model.count_params()}")
+    # print(f"Model Summary: {model.summary()}")
