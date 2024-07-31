973ac3b6070a7e5fd8dfa0bcfc56db03dbcb7cdd
Andrew Fox <apfox500@gmail.com>
2024-07-31 15:27:06 -0600

    added tiling ability

--- pipeline/Postprocessing.py
+++ pipeline/Postprocessing.py
@@ -1,5 +1,6 @@
 import rasterio
 import matplotlib.pyplot as plt
+import numpy as np
 
 def save_image(img, meta, filepath):
     """
@@ -34,3 +35,31 @@ def display_both(img, mask):
     ax[1].imshow(mask, cmap='gray')
     ax[1].axis('off')
     plt.show()
+
+def combine_tiles(tiles, meta, tile_sizes,  output_filepath):
+    """
+    Combine the list of tiles into a single image and save it.
+
+    Args:
+        tiles (list): A list of numpy arrays representing the tiles.
+        meta (dict): The metadata for the image.
+        output_filepath (str): The path to the output file.
+
+    Returns:
+        None
+    """
+    # combine the tiles
+    height = tile_sizes[0]
+    width = tile_sizes[1]
+
+    grid = np.reshape(tiles, (height, width, 1024, 1024))
+
+
+    large_array = np.block([[grid[i, j] for j in range(29)] for i in range(24)])
+
+    pred = np.expand_dims(large_array, axis=0) # add dimension
+
+    # save the image
+    save_image(pred, meta, output_filepath)
+
+    return pred
--- pipeline/Prediction.py
+++ pipeline/Prediction.py
@@ -1,14 +1,15 @@
 import pickle
 import os
-
+import matplotlib as plt
 from sklearn.ensemble import RandomForestClassifier 
+from tqdm import tqdm
 # TODO imports for nn models
 
-from Preprocessing import nn_preprocessing, rf_preprocessing
-from Postprocessing import save_image, display_both
+from Preprocessing import nn_preprocessing, rf_preprocessing, rf_preprocessing_tiled
+from Postprocessing import save_image, display_both, combine_tiles
 
 IMG_FILEPATH = 'C:/Users/apfox/UCB-O365/Travis Hainsworth - RMBL/2 - Source Data/2019-2020_Data/Imagery/DeerCreekTrail/DeerCreekTrail_2019_05_22_snow.tif' # change this filepath if running Prediciton.py
-OUTPUT_DIRECTORY = "./predictions/" # change this directory if running Prediction.py
+OUTPUT_DIRECTORY = "./predictions/" # change this directory if running 
 
 def predict_nn(model, img):
     """
@@ -42,7 +43,7 @@ def predict_rf(model, img_df, verbose = True):
 
     return model.predict(img_df)
 
-def full_pipeline(img_filepath, model_type, output_dir, verbose=False, **kwargs):
+def full_pipeline(img_filepath, model_type, output_dir, verbose=False, tile=False, **kwargs):
     """
     Runs the full pipeline for image prediction given an image and a model.
     It is highly reccomended to scale down the image if using a Random Forest model for time efficiency.
@@ -84,43 +85,77 @@ def full_pipeline(img_filepath, model_type, output_dir, verbose=False, **kwargs)
     if not img_filepath.endswith('.tif'):
         raise ValueError(f"Image must be a .tif file(in GeoTIFF format)")
 
-    # preprocess image
-    if model_type == 'RF':
-        img, img_meta = rf_preprocessing(img_filepath, verbose=verbose, **kwargs)
-    else:
-        img, img_meta = nn_preprocessing(img_filepath, **kwargs)
+    if not tile:
 
-    # check it is GeoTIFF
-    if img_meta['driver'] != 'GTiff':
-        raise ValueError("Image must be in GeoTIFF format")
+        # preprocess image
+        if model_type == 'RF':
+            img, img_meta = rf_preprocessing(img_filepath, verbose=verbose, **kwargs)
+        else:
+            img, img_meta = nn_preprocessing(img_filepath, **kwargs)
+
+        # check it is GeoTIFF
+        if img_meta['driver'] != 'GTiff':
+            raise ValueError("Image must be in GeoTIFF format")
+        
+        # predict with choosen model
+        if model_type == 'RF':
+            # get prediciton
+            pred = predict_rf(model, img, verbose=verbose)
+
+            # reshape prediction
+            pred = pred.reshape((img_meta['height'], img_meta['width'], 1))
+            pred = pred.transpose((2, 0, 1))
+        else:
+            pred = predict_nn(model, img)
+
+        # update metadata to one channel
+        img_meta['count'] = 1
+
+        # make output_filepath
+        basename = os.path.basename(img_filepath)
+        output_filepath = os.path.join(output_dir, f"{basename}_{model_type}_prediction.tif")
+
+        # save image
+        save_image(pred, img_meta, output_filepath)
+
+        if verbose:
+            # get the red, green and blue columns and reshape
+            img_rgb = img[['Blue', 'Green', 'Red']].values.reshape((img_meta['height'], img_meta['width'], 3))
+
+            # display the image and the mask
+            display_both(img_rgb, pred[0])
     
-    # predict with choosen model
-    if model_type == 'RF':
-        # get prediciton
-        pred = predict_rf(model, img, verbose=verbose)
+    else: 
+        if model_type == 'RF':
+            tiles = []
+            generator, img_meta, tile_sizes = rf_preprocessing_tiled(img_filepath, verbose=verbose)
+            for tile in tqdm(generator, unit='tile'):
+                # get prediciton
+                pred = predict_rf(model, tile, verbose=verbose)
 
-        # reshape prediction
-        pred = pred.reshape((img_meta['height'], img_meta['width'], 1))
-        pred = pred.transpose((2, 0, 1))
-    else:
-        pred = predict_nn(model, img)
+                # reshape prediction
+                pred = pred.reshape((1024, 1024, 1))
+                pred = pred.transpose((2, 0, 1))
 
-    # update metadata to one channel
-    img_meta['count'] = 1
+                # add prediction to output list
+                tiles.append(pred)
+            
+            # combine tiles
 
-    # make output_filepath
-    basename = os.path.basename(img_filepath)
-    output_filepath = os.path.join(output_dir, f"{basename}_{model_type}_prediction.tif")
+            # make output_filepath
+            basename = os.path.basename(img_filepath)
+            output_filepath = os.path.join(output_dir, f"{basename}_{model_type}_prediction.tif")
 
-    # save image
-    save_image(pred, img_meta, output_filepath)
+            img_meta['count'] = 1
+            with open('tiles.pkl', 'wb') as f:
+                pickle.dump((tiles, img_meta, tile_sizes, output_filepath), f)
+            pred_img = combine_tiles(tiles, img_meta, tile_sizes, output_filepath)
 
-    if verbose:
-        # get the red, green and blue columns and reshape
-        img_rgb = img[['Blue', 'Green', 'Red']].values.reshape((img_meta['height'], img_meta['width'], 3))
-
-        # display the image and the mask
-        display_both(img_rgb, pred[0])
+            if verbose:
+                #display mask with maptpltlib
+                plt.imshow(pred_img[0], cmap='gray')
+                plt.show() 
+    
 
 if __name__ == '__main__':
     # make output_dir
@@ -128,5 +163,5 @@ if __name__ == '__main__':
         os.mkdir(OUTPUT_DIRECTORY)
 
     # run full pipeline
-    full_pipeline(IMG_FILEPATH, 'RF', OUTPUT_DIRECTORY, verbose=True, width=1000)
+    full_pipeline(IMG_FILEPATH, 'RF', OUTPUT_DIRECTORY, verbose=True, tile=True)
     # full_pipeline(IMG_FILEPATH, 'U-NET', OUTPUT_DIRECTORY)
